a0279d6aa2013670c42ddca2b927982e765aed26
Michael Hallock <mike@hallock.net>
2019-07-07 03:33:38 -0400

    Cleaned up a few pieces

--- castv2-sender.js
+++ castv2-sender.js
@@ -2,7 +2,7 @@ module.exports = function(RED) {
     "use strict";
     const Client = require("castv2-client").Client;
     const DefaultMediaReceiver = require("castv2-client").DefaultMediaReceiver;
-    const googletts = require('google-tts-api');
+    const googletts = require("google-tts-api");
 
     function CastV2SenderNode(config) {
         RED.nodes.createNode(this, config);
@@ -79,8 +79,8 @@ module.exports = function(RED) {
                     receiver.play(node.onStatus);
                     break;
                 case "SEEK":
-                    if (command.currentTime) {
-                        receiver.seek(command.currentTime, node.onStatus);
+                    if (command.time) {
+                        receiver.seek(command.time, node.onStatus);
                     }
                     break;
                 case "STOP":
@@ -99,13 +99,13 @@ module.exports = function(RED) {
                         if (Array.isArray(command.media)) {
                             // Queue handling
                             receiver.queueLoad(
-                                command.media,
+                                command.media.map(node.buildMediaObject),
                                 { startIndex: 1, repeatMode: "REPEAT_OFF" },
                                 node.onStatus);
                         } else {
                             // Single media handling
                             receiver.load(
-                                command.media,
+                                node.buildMediaObject(command.media),
                                 { autoplay: true },
                                 node.onStatus);
                         }
@@ -118,12 +118,11 @@ module.exports = function(RED) {
 
                         // Get castable URL
                         googletts(command.text, language, speed).then(url => {
-                            let media = {
-                                contentId: url,
+                            let media = node.buildMediaObject({
+                                url: url,
                                 contentType: "audio/mp3",
-                                imageUrl: "https://nodered.org/node-red-icon.png",
-                                contentTitle: command.contentTitle ? command.contentTitle : "tts"
-                            };
+                                title: command.title ? command.title : "tts"
+                            });
 
                             receiver.load(
                                 media,
@@ -195,6 +194,87 @@ module.exports = function(RED) {
             let status = node.context().get("status");
             node.send({ payload: status });
         });
+
+        /*
+         * Build a media object
+         */
+        this.buildMediaObject = function(media) {
+            let fileName = media.contentId.split("/")[-1].split("?"[0]);
+            return {
+                contentId : media.url,
+                contentType: media.contentType || node.getContentType(fileName),
+                streamType: media.streamType || "BUFFERED",
+                metadata: {
+                    metadataType: 0,
+                    title: media.title || fileName,
+                    subtitle: null,
+                    images: [
+                        { url: media.image || "https://nodered.org/node-red-icon.png" }
+                    ]
+                }
+            };
+        };
+
+        /*
+         * Get content type for a URL
+         */
+        this.getContentType = function(fileName) {
+            const contentTypeMap = {
+                "3gp": "video/3gpp",
+                aac: "video/mp4",
+                aif: "audio/x-aiff",
+                aiff: "audio/x-aiff",
+                aifc: "audio/x-aiff",
+                avi: "video/x-msvideo",
+                au: "audio/basic",
+                bmp: "image/bmp",
+                flv: "video/x-flv",
+                gif: "image/gif",
+                ico: "image/x-icon",
+                jpe: "image/jpeg",
+                jpeg: "image/jpeg",
+                jpg: "image/jpeg",
+                m3u: "audio/x-mpegurl",
+                m3u8: "application/x-mpegURL",
+                m4a: "audio/mp4",
+                mid: "audio/mid",
+                midi: "audio/mid",
+                mov: "video/quicktime",
+                movie: "video/x-sgi-movie",
+                mpa: "audio/mpeg",
+                mp2: "audio/x-mpeg",
+                mp3: "audio/mp3",
+                mp4: "audio/mp4",
+                mjpg: "video/x-motion-jpeg",
+                mjpeg: "video/x-motion-jpeg",
+                mpe: "video/mpeg",
+                mpeg: "video/mpeg",
+                mpg: "video/mpeg",
+                ogg: "audio/ogg",
+                ogv: "audio/ogg",
+                png: "image/png",
+                qt: "video/quicktime",
+                ra: "audio/vnd.rn-realaudio",
+                ram: "audio/x-pn-realaudio",
+                rmi: "audio/mid",
+                rpm: "audio/x-pn-realaudio-plugin",
+                snd: "audio/basic",
+                stream: "audio/x-qt-stream",
+                svg: "image/svg",
+                tif: "image/tiff",
+                tiff: "image/tiff",
+                vp8: "video/webm",
+                wav: "audio/vnd.wav",
+                webm: "video/webm",
+                webp: "image/webp",
+                wmv: "video/x-ms-wmv"
+            };
+
+            let ext = fileName.split(".")[-1];
+            contentType = contentTypeMap[ext.toLowerCase()];
+
+            return contentType || "audio/basic";
+        };
     }
 
     RED.nodes.registerType("castv2-sender", CastV2SenderNode);
